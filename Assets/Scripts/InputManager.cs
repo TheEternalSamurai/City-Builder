f9fc7673a2b196a2e1f46b7751778bbbb0abfd8d
TheEternalSamurai <samthesonicfan357@gmail.com>
2020-01-31 20:42:40 -0600

    Added Building Placement Behaviour

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     public LayerMask mouseInputMask;
+    public GameObject buildingPrefab;
+    public int cellSize = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +29,22 @@ public class InputManager : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
             {
-                Vector3 pos = hit.point - transform.position;
-                Debug.Log(pos);
+                Vector3 position = hit.point - transform.position;
+                Debug.Log(CalculateGridPosition(position));
+                CreateBuilding(CalculateGridPosition(position));
             }
         }
+    }
+
+    public Vector3 CalculateGridPosition(Vector3 inputPosition)
+    {
+        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
+        return new Vector3(x * cellSize, 0, z * cellSize);
+    }
+
+    private void CreateBuilding(Vector3 gridPosition)
+    {
+        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
     }
 }
