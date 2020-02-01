5bb6f1b419e464a0196d2e3bf165ad9346c66856
TheEternalSamurai <samthesonicfan357@gmail.com>
2020-01-31 23:02:44 -0600

    Extracted Grid Calculation Logic into GridStructure.cs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,16 +6,8 @@ using UnityEngine.EventSystems;
 public class InputManager : MonoBehaviour
 {
     public LayerMask mouseInputMask;
-    public GameObject buildingPrefab;
-    public int cellSize = 3;
+    //public GameObject buildingPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         GetInput();
@@ -30,21 +22,13 @@ public class InputManager : MonoBehaviour
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
             {
                 Vector3 position = hit.point - transform.position;
-                Debug.Log(CalculateGridPosition(position));
-                CreateBuilding(CalculateGridPosition(position));
             }
         }
     }
 
-    public Vector3 CalculateGridPosition(Vector3 inputPosition)
-    {
-        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
-        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
-        return new Vector3(x * cellSize, 0, z * cellSize);
-    }
-
+    /*
     private void CreateBuilding(Vector3 gridPosition)
     {
         Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
-    }
+    }*/
 }
