debd549c91950e310e9d6e0c9b5a901116551029
TheEternalSamurai <samthesonicfan357@gmail.com>
2020-02-01 22:01:43 -0600

    Added Build and Cancle Build Button Listeners

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Button buildResidentialAreaBtn;
+    public Button cancleActionBtn;
+    public GameObject cancleActionPanel;
+
+    private Action OnBuildAreaHandler;
+    private Action OnCancleActionHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cancleActionPanel.SetActive(false);
+        buildResidentialAreaBtn.onClick.AddListener(OnBuildAreaCallback);
+        cancleActionBtn.onClick.AddListener(OnCancleActionCallback);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnBuildAreaCallback()
+    {
+        cancleActionPanel.SetActive(true);
+        OnBuildAreaHandler?.Invoke();
+    }
+
+    private void OnCancleActionCallback()
+    {
+        cancleActionPanel.SetActive(false);
+        OnCancleActionHandler?.Invoke();
+    }
+
+    public void AddListenerOnBuildAreaEvent(Action listener)
+    {
+        OnBuildAreaHandler += listener;
+    }
+
+    public void RemoveListenerOnBuildAreaEvent(Action listener)
+    {
+        OnBuildAreaHandler -= listener;
+    }
+
+    public void AddListenerOnCancleActionEvent(Action listener)
+    {
+        OnCancleActionHandler += listener;
+    }
+
+    public void RemoveListenerOnCancleActionEvent(Action listener)
+    {
+        OnCancleActionHandler -= listener;
+    }
+}
