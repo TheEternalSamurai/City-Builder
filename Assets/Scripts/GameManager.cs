5499e292805b9f0346e37e6e161a42bb1198d07b
TheEternalSamurai <samthesonicfan357@gmail.com>
2020-02-01 23:15:48 -0600

    Extracted Input Manager Delegates to IInputManager Interface

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public PlacementManager placementManager;
-    public InputManager inputManager;
+    public IInputManager inputManager;
+    public UIController uiController;
     public int width, length;
 
     private GridStructure grid;
     private int cellSize = 3;
+    private bool buildingModeActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        inputManager = FindObjectsOfType<MonoBehaviour>().OfType<IInputManager>().FirstOrDefault();
         grid = new GridStructure(cellSize, width, length);
         inputManager.AddListenerOnPointerDownEvent(HandleInput);
+        uiController.AddListenerOnBuildAreaEvent(StartPlacementMode);
+        uiController.AddListenerOnCancleActionEvent(CancleAction);
     }
 
     // Update is called once per frame
@@ -25,10 +31,20 @@ public class GameManager : MonoBehaviour
         
     }
 
+    private void StartPlacementMode()
+    {
+        buildingModeActive = true;
+    }
+
+    private void CancleAction()
+    {
+        buildingModeActive = false;
+    }
+
     private void HandleInput(Vector3 position)
     {
         Vector3 gridPosition = grid.CalculateGridPosition(position);
-        if (!grid.IsCellTaken(gridPosition))
+        if (buildingModeActive && !grid.IsCellTaken(gridPosition))
         {
             placementManager.CreateBuilding(gridPosition, grid);
         }
---
+++ Assets/Scripts/IInputManager.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+public interface IInputManager
+{
+    void AddListenerOnPointerDownEvent(Action<Vector3> listener);
+    void RemoveListenerOnPointerDownEvent(Action<Vector3> listener);
+}
