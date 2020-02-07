4d95a3f3db72a0be006f513907ba53e0fa98678e
TheEternalSamurai <samthesonicfan357@gmail.com>
2020-02-06 20:24:16 -0600

    Added Unit Tests for StructureRepository

--- Assets/PlayModeTests/StructureRepositoryTest.cs
+++ Assets/PlayModeTests/StructureRepositoryTest.cs
@@ -8,17 +8,69 @@ namespace Tests
 {
     public class StructureRepositoryTest
     {
+        StructureRepository repository;
+
         [OneTimeSetUp]
         public void Init()
         {
+            CollectionSO collection = new CollectionSO();
+            
+            var road = new RoadStructureSO();
+            road.buildingName = "Road";
+            collection.roadStructure = road;
+
+            var facility = new SingleFacilitySO();
+            facility.buildingName = "PowerPlant";
+            collection.singleStructureList = new List<SingleStructureBaseSO>();
+            collection.singleStructureList.Add(facility);
+
+            var zone = new ZoneStructureSO();
+            zone.buildingName = "Commercial";
+            collection.zonesList = new List<ZoneStructureSO>();
+            collection.zonesList.Add(zone);
+
+            GameObject testObject = new GameObject();
+            repository = testObject.AddComponent<StructureRepository>();
+            repository.modelDataCollection = collection;
         }
-  
+
         [UnityTest]
-        public IEnumerator StructureRepositoryTestWithEnumeratorPasses()
+        public IEnumerator StructureRepositoryTestRoadNamePasses()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            string name = repository.GetRoadStructureName();
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual("Road", name);
+        }
+
+        [UnityTest]
+        public IEnumerator StructureRepositoryTestSingleStructureListQuantityPasses()
+        {
+            int quantity = repository.GetSingleStructureNames().Count;
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual(1, quantity);
+        }
+
+        [UnityTest]
+        public IEnumerator StructureRepositoryTestSingleStructureListNamePasses()
+        {
+            string name = repository.GetSingleStructureNames()[0];
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual("PowerPlant", name);
+        }
+        [UnityTest]
+        public IEnumerator StructureRepositoryTestZoneListQuantityPasses()
+        {
+            int quantity = repository.GetZoneNames().Count;
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual(1, quantity);
+        }
+
+        [UnityTest]
+        public IEnumerator StructureRepositoryTestZoneListNamePasses()
+        {
+            string name = repository.GetZoneNames()[0];
+            yield return new WaitForEndOfFrame();
+            Assert.AreEqual("Commercial", name);
         }
     }
 }
